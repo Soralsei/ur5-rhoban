746bf62c2f52135aff933ae9d24a6848ff4ff0a5
Kohio DEFLESSELLE <kohiodef@gmail.com>
2024-09-10 14:56:34 +0200

    Format path_test

--- src/ur5_kinematics/src/ur5_kinematics/testing/path_test.py
+++ src/ur5_kinematics/src/ur5_kinematics/testing/path_test.py
@@ -8,8 +8,17 @@ from ur5_kinematics.testing import BaseTest
 from ur5_kinematics.msg import URGoToResult
 from control_msgs.msg import FollowJointTrajectoryGoal, FollowJointTrajectoryResult
 
+
 class PathTest(BaseTest):
-    def __init__(self, controller_topic: str, kinematics_topic:str, path: Iterable, prefix:str = '', loop: bool = True, segment_duration: float = 0.0):
+    def __init__(
+        self,
+        controller_topic: str,
+        kinematics_topic: str,
+        path: Iterable,
+        prefix: str = "",
+        loop: bool = True,
+        segment_duration: float = 0.0,
+    ):
         super(PathTest, self).__init__(controller_topic, kinematics_topic, prefix)
         self.path = path
         self.segment_duration = segment_duration
@@ -18,22 +27,21 @@ class PathTest(BaseTest):
         else:
             self.path = iter(path)
 
-            
     def update(self):
         target = next(self.path)
         goal = self.init_goal(target)
         self.kinematics_client.send_goal_and_wait(goal)
-        
+
         result: URGoToResult = self.kinematics_client.get_result()
         if result.state == URGoToResult.SUCCEEDED:
             traj_goal = FollowJointTrajectoryGoal()
             traj_goal.trajectory = result.trajectory
-            
-            rospy.loginfo(f'Got trajectory, executing...')
+
+            rospy.loginfo(f"Got trajectory, executing...")
             self.controller_client.send_goal_and_wait(traj_goal)
             res: FollowJointTrajectoryResult = self.controller_client.get_result()
 
             if res.error_code == FollowJointTrajectoryResult.SUCCESSFUL:
-                rospy.loginfo(f'Trajectory execution success')
+                rospy.loginfo(f"Trajectory execution success")
         else:
-            raise RuntimeError('Solver failed')
+            raise RuntimeError("Solver failed")
